e168fd495e44500f4bab4e5f24745d6fc3d4e3c9
ozodbek2410 <ozodbekmirgasimov@gmail.com>
2025-09-27 10:59:10 +0500

    chore(backend): remove unused productRoutesFast duplicate (fast is handled in productRoutes.js)

--- backend/routes/productRoutesFast.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getProductsFast } = require('../controllers/productControllerOptimized');
-
-const router = express.Router();
-
-// Ultra-fast products endpoint
-router.get('/fast', getProductsFast);
-
-module.exports = router;
